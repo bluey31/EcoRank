9f841888d9bd510e4fae0acdd0dad60056424873
Lewis Bell <lewisbell999@gmail.com>
2017-02-27 21:17:58 +0000

    users

--- Backend/users.php
+++ Backend/users.php
@@ -5,3 +5,8 @@
  * Date: 27/02/2017
  * Time: 20:59
  */
+require_once "functions.php";
+
+echo json_encode(run_db(function($db){
+    return sqlquery($db,"SELECT userId from Users",[],SQL_SINGLE|SQL_MULTIPLE);
+}));
