7cf89c7b2b7d8dfc0071e816d2c841190ef2caff
Charlie Harding <xsanda@me.com>
2017-02-27 13:39:16 +0000

    Backend: return num array

--- Backend/main.php
+++ Backend/main.php
@@ -5,6 +5,9 @@
  * Date: 27/02/2017
  * Time: 12:55
  */
-for($i =0; $i < 10; $i+=1){
-    print($i.PHP_EOL);
-}
+
+$array = [];
+for($i =0; $i < 10; $i+=1) $array[] = $i;
+
+header('Content-Type: application/json');
+echo json_encode($array);
