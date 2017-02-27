57df6bf4ae49e1ca98feaca416cb02f664778d89
Jay Lees <jay.lees@me.com>
2017-02-27 14:31:32 +0000

    Design of first VC

--- iOS App/EcoRank/EcoRank/ERViewController.swift
+++ iOS App/EcoRank/EcoRank/ERViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var grassHillView: UIView!
+    @IBOutlet weak var logoLabel: UILabel!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var signUpButton: UIButton!
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // The sky is always blue
-        self.view.backgroundColor = ERSkyBlue
+        setupView()
     }
 
+    func setupView(){
+        self.view.backgroundColor = ERSkyBlue
+        grassHillView.backgroundColor = ERGreen
+        logoLabel.font = UIFont(name: "Montserrat-Medium", size: 40)
+        logoLabel.textColor = UIColor.white
+        logoLabel.textAlignment = .center
+        loginButton.titleLabel?.font = UIFont(name: "Montserrat-Medium", size: 28)
+        signUpButton.titleLabel?.font = UIFont(name: "Montserrat-Medium", size: 20)
+    }
 
+    @IBAction func userTouchedLoginButton(_ sender: Any) {
+        UIView.performWithoutAnimation {
+            performSegue(withIdentifier: "segueToLoginScreen", sender: sender)
+        }
+    }
 }
 
