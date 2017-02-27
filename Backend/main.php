97b7b72587eb38f5526cca4706073f2ed0bccb5b
Charlie Harding <xsanda@me.com>
2017-02-27 13:44:54 +0000

    Add max to JSON text

--- Backend/main.php
+++ Backend/main.php
@@ -7,7 +7,7 @@
  */
 
 $array = [];
-for($i =0; $i < 10; $i+=1) $array[] = $i;
+for($i = 0; $i < $_GET['max'] ?? 10; $i++) $array[] = $i;
 
 header('Content-Type: application/json');
 echo json_encode($array);
