171f35d8f8762ca22d87675de82f6bc2d0ef73d3
Lewis Bell <lewisbell999@gmail.com>
2017-02-27 18:09:24 +0000

    time echos on hash generating

--- Backend/functions.php
+++ Backend/functions.php
@@ -21,8 +21,10 @@ function hashPassword($password, $salt){
 }
 
 function newSalt(){
+    echo time();
     $cost = 20;
     $salt = strtr(base64_encode(random_bytes(16)), '+', '.');
     $salt = sprintf("$2a$%02d$", $cost) . $salt;
+    echo time();
     return $salt;
 }
