ecedcf93c1d6f92850bc9c9008deedda706038ff
Charlie Harding <xsanda@me.com>
2017-02-27 13:48:40 +0000

    Fix ?: further

--- Backend/main.php
+++ Backend/main.php
@@ -7,7 +7,8 @@
  */
 
 $array = [];
-for($i = 0; $i < $_GET['max'] ?: 10; $i++) $array[] = $i;
+$max = (int) $_GET['max'];
+for($i = 0; $i < $max ?: 10; $i++) $array[] = $i;
 
 header('Content-Type: application/json');
 echo json_encode($array);
